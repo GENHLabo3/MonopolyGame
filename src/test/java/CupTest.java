b585dc68815738f881b85f017444d4bcddc86ecb
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-04-02 11:40:19 +0200

    [TEST] Negative number of dice

--- src/test/java/CupTest.java
+++ src/test/java/CupTest.java
@@ -1,18 +1,29 @@
 package test.java;
 
 import main.java.Cup;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class CupTest {
 
-    @Test
+    @RepeatedTest(20)
     void cupRollsValue() {
         Cup cup = new Cup(2);
         cup.roll();
         assertTrue(2 <= cup.getTotal() && cup.getTotal() <= 12);
     }
 
+    @Test
+    void negativeNumberOfDice() {
+        try{
+            Cup cup = new Cup(-1);
+            fail("Exception not thrown");
+        }catch(Exception e){
+            // OK an exception has been thrown
+        }
+    }
 
 }
