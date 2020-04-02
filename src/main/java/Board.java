e0d89c0340800824eab58352fab37cfc7a232b64
teo <ferraricorneloup.teo@gmail.com>
2020-04-02 10:45:08 +0200

    first commit

---
+++ src/main/java/Board.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Board {
+
+    public Board() {
+        for (int i = 0; i < 40; i++){
+
+        }
+    }
+
+
+    private ArrayList<Square> squares;
+}
---
+++ src/main/java/Square.java
@@ -0,0 +1,13 @@
+public class Square {
+
+    public Square(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+
+}
