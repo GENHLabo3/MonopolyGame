7e94ad9fd768e802114f25284c82d571c6bc9525
Juri <Chau-ying-kot@hotmail.com>
2020-04-15 17:15:02 +0200

    Add test

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -9,12 +9,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlayerTest {
     private static ArrayList<Die> dice = new ArrayList<>();
     private static Board board = new Board();
+    private static Player player1;
+
 
     @BeforeAll
     public static void init(){
         dice.add(new Die());
         dice.add(new Die());
-        Player player1 = new Player("Joe", new Piece(PiecesName.Penguin),board, dice);
+        player1 = new Player("Joe", new Piece(PiecesName.Penguin),board, dice);
     }
 
     @Test
@@ -22,5 +24,23 @@ class PlayerTest {
         Assertions.assertThrows(IllegalArgumentException.class, ()->
                 new Player("Joe", new Piece(PiecesName.Penguin), board, dice)
         );
+    }
+
+    @Test
+    public void testSetLocation(){
+        player1.setLocation(new Square("Go"));
+        Assertions.assertEquals("Go", player1.getPiece().getLocation().getName());
+    }
+
+    @Test
+    public void add100Cash(){
+        player1.addCash(100);
+        Assertions.assertEquals(1600, player1.getNetWorth());
+    }
+
+    @Test
+    public void reduce100Cash(){
+        player1.reduceCash(100);
+        Assertions.assertEquals(1400, player1.getNetWorth());
     }
 }
