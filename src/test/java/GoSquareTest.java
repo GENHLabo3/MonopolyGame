f8086538133372cbeb6ccc148d8b29a54c544b50
teo <ferraricorneloup.teo@gmail.com>
2020-04-16 12:18:24 +0200

    Sorry  travis I won't do it again

--- src/test/java/GoSquareTest.java
+++ src/test/java/GoSquareTest.java
@@ -10,7 +10,7 @@ class GoSquareTest {
         Board  board = new Board();
         Cup cup = new Cup(2);
         Player p = new Player("Joe", new Piece(PiecesName.RaceCar), board, cup);
-       Square square = new GoSquare("test");
+       Square square = new GoSquare();
        square.landedOn(p);
        assertEquals(1700, p.getNetWorth());
     }
