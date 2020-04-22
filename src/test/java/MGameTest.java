bf64ebe0e1f7447ebd8371f980a2f8fd8a1c1f59
Juri <Chau-ying-kot@hotmail.com>
2020-04-22 10:03:08 +0200

    Add test in MGame, check that the player position and cash is correct

--- src/test/java/MGameTest.java
+++ src/test/java/MGameTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,15 +7,32 @@ class MGameTest {
 
     @Test
     void checkPlayerSmallerThat2() {
-        assertThrows(IllegalArgumentException.class, ()->
-               new MGame(1)
+        assertThrows(IllegalArgumentException.class, () ->
+                new MGame(1)
         );
     }
 
     @Test
     void checkPlayerBiggerThat8() {
-        assertThrows(IllegalArgumentException.class, ()->
+        assertThrows(IllegalArgumentException.class, () ->
                 new MGame(10)
+        );
+    }
+
+    @Test
+    @DisplayName("Player position is on GoSquare and had a 200 income")
+    void landedOnGoSquare() {
+        Board board = new Board();
+        Cup cup = new Cup(2);
+        Player player = new Player("Player", board, cup);
+
+        int playerOldCash = player.getNetWorth();
+
+        board.getFirstSquare().landedOn(player);
+
+        assertAll(
+                () -> assertSame(player.getLocation(), board.getFirstSquare()),
+                () -> assertEquals(playerOldCash + GoSquare.CASH_TO_ADD, player.getNetWorth())
         );
     }
 }
